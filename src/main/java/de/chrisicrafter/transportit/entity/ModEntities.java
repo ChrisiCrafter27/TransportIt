2afb32695ff7bfab45c6f59abe7c61a5eaa1c2fc
BuildTools <unconfigured@null.spigotmc.org>
2023-11-10 20:16:01 +0100

    Initial Commit

--- src/main/java/de/chrisicrafter/transportit/entity/ModEntities.java
+++ src/main/java/de/chrisicrafter/transportit/entity/ModEntities.java
@@ -10,11 +10,11 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModEntities {
-    public static final DeferredRegister<EntityType<?>> VANILLA_ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, TransportIt.MOD_ID);
+    public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, TransportIt.MOD_ID);
 
-    public static final RegistryObject<EntityType<CustomMinecartFurnace>> CUSTOM_MINECART_FURNACE = VANILLA_ENTITY_TYPES.register("furnace_minecart", () -> EntityType.Builder.<CustomMinecartFurnace>of(CustomMinecartFurnace::new, MobCategory.MISC).sized(0.98F, 0.7F).clientTrackingRange(8).build("furnace_minecart"));
+    public static final RegistryObject<EntityType<CustomMinecartFurnace>> CUSTOM_MINECART_FURNACE = ENTITY_TYPES.register("furnace_minecart", () -> EntityType.Builder.<CustomMinecartFurnace>of(CustomMinecartFurnace::new, MobCategory.MISC).sized(0.98F, 0.7F).clientTrackingRange(8).build("furnace_minecart"));
 
     public static void register(IEventBus eventBus) {
-        eventBus.register(VANILLA_ENTITY_TYPES);
+        eventBus.register(ENTITY_TYPES);
     }
 }
